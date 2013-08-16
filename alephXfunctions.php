5ea9bf03f878dc2cb4ccbcbb3b5b1983e9719457
spurioso <spurioso@gmail.com>
2013-08-15 23:17:10 -0400

    added error handling, simplified functions

--- alephXfunctions.php
+++ alephXfunctions.php
@@ -1,25 +1,24 @@
 <?php
-/* Returns a set number for a given request. $request and $code should be passed
- * into the function. $base and $op default to "CP" and "find." To chance to a different base,
- * say for Towson, pass in a third parameter when calling the function. (Need to document where
- * the base codes are.) alephFind returns $alephSetNumber, which can be used in alephPresent
- * to retrieve corresponding MarcXML records.  
+
+/*Function alephFind sends an Aleph "find" request to Aleph X-Services (the Aleph API).
+ * If successful, the Find request returns the number of results for the query, a unique number for the set
+ * of results ("set_number"), and some other things. If unsuccessful, Find returns an error message.
+ * An X-Services "Present" request returns MARCXML for a given set_entry in a set_number. The alephPresent function
+ * handles this step.
  */
 function alephFind($request, $code, $base = "CP", $op = "find") {
 	/* Build the url for Aleph x-services find request. Returns set number if successful */	
-	$findURL = "http://catalog.umd.edu/X?request=".$request."&op=".$op."&code=".$code."&base=".$base;
-	$findResults = file_get_contents($findURL); // get results of find request...	
-	$alephFindXML = new SimpleXMLElement($findResults); //...and turn the results into an XML object
-    if ($alephFindXML->set_number) {        //check for set number
-        $alephSetNumber = $alephFindXML->set_number; //if there is a set number, store it in a variable
-        return($alephSetNumber);               
-    } elseif ($alephFindXML->error) {        //error is returned if barcode not found
-        return("Barcode not found <br />");        
-    } else {
-        return("Something went horribly wrong <br />"); //handles other unforseen errors       
-        
-    } // end if
-} // end alephFind
+	$findURL = "http://catalog.umd.edu/X?request=".$request."&op=".$op."&code=".$code."&base=".$base;	
+	if (file_get_contents($findURL) === False) { // something went wrong with the HTTP request or other unforseen errors occurred
+		$error = "<item><error>unknown error</error></item>";     	
+     	$alephFindXML = new SimpleXMLElement($error);
+        return($alephFindXML); 
+	} else {
+		$findResults = file_get_contents($findURL); // get results of find request
+		$alephFindXML = new SimpleXMLElement($findResults); // turn the results into an XML object
+		return($alephFindXML);	
+	} // end if
+} // end alephFind		
 
 function alephPresent($alephSetNumber, $setEntry = 1){
 	/*present url gets the actual MARC XML file for the results set
--- getMARC.php
+++ getMARC.php
@@ -1,16 +1,23 @@
 <?php
 
-include "alephXfunctions.php";
+require "alephXfunctions.php";
 
 /* Store the Aleph system number in a variable. Works also without the initial zeros.
  * I'm treating the number as a string in this example. 
  * Need to remove the initial zeroes otherwise.  */
-$alephNumbers = array ("001450414", "003778295"); // sample aleph numbers for testing  
+$alephNumbers = array ("12345678", "001450414", "003778295"); // sample aleph numbers for testing  
 $alephNum = $alephNumbers[1];
-$code = "SYS"; 
-$setNum = alephFind($alephNum, $code); // calls the alephFind function in alephXfunctions.php
-$alephMarcXML = alephPresent($setNum); // calls the alephPresent function in alephXfunctions.php
+$code = "SYS"; // code for X-services. "SYS" is for system number. change to "bar" for barcode, etc. 
 
-print_r($alephMarcXML); // for testing
+$alephFindResults = alephFind($alephNum, $code); // calls the alephFind function in alephXfunctions.php
+
+// test results for erros
+if ($alephFindResults->error) {
+	echo "An error occurred: ".$alephFindResults->error."</br>";	 
+} else {
+	$setNum = $alephFindResults->set_number;	
+	$alephMarcXML = alephPresent($setNum); // calls the alephPresent function in alephXfunctions.php
+	print_r($alephMarcXML); // this line for testing. Remove for production. 
+}// end if
    
 ?>
