3b3139d27b1e0d26509f81e16eff026570c8904b
spurioso <spurioso@gmail.com>
2013-09-14 10:17:08 -0400

    Cleaned up functions in alephXfunctions.php

--- alephXfunctions.php
+++ alephXfunctions.php
@@ -1,45 +1,39 @@
 <?php
 
-/*Function alephFind sends an Aleph "find" request to Aleph X-Services (the Aleph API).
- * If successful, the Find request returns the number of results for the query, a unique number for the set
- * of results ("set_number"), and some other things. If unsuccessful, Find returns an error message.
- * An X-Services "Present" request returns MARCXML for a given set_entry in a set_number. The alephPresent function
- * handles this step.
- */
- 
-function buildFindURL($request, $code, $base = "CP", $op = "find") {
+// Build a url for use in the alephXfind() function. Requires a $request and a $code (i.e. "BAR")  
+function alephXbuildFindURL($request, $code, $base = "CP", $op = "find") {
 	$findURL = "http://catalog.umd.edu/X?request=".$request."&op=".$op."&code=".$code."&base=".$base;
 	return($findURL);
 }
 
-function buildPresentURL($alephSetNumber, $setEntry = 1) {
-	$presentURL = "http://catalog.umd.edu/X?set_no=".$alephSetNumber."&set_entry=".$setEntry."&op=present";
+// Build a url for use in the alephXpresent() function. Requires the $setNum returned by alephXgetSetNum()
+function alephXbuildPresentURL($setNum, $setEntry = 1) {
+	$presentURL = "http://catalog.umd.edu/X?set_no=".$setNum."&set_entry=".$setEntry."&op=present";
 	return($presentURL);
 }
 
-function alephFind($request, $code, $base = "CP", $op = "find") {
-	/* Build the url for Aleph x-services find request. Returns set number if successful */	
-	$findURL = "http://catalog.umd.edu/X?request=".$request."&op=".$op."&code=".$code."&base=".$base;	
-	if (file_get_contents($findURL) === False) { // something went wrong with the HTTP request or other unforseen errors occurred
-		$error = "<item><error>unknown error</error></item>";     	
-     	$alephFindXML = new SimpleXMLElement($error);
-        return($alephFindXML); 
-	} else {
-		$findResults = file_get_contents($findURL); // get results of find request
-		$alephFindXML = new SimpleXMLElement($findResults); // turn the results into an XML object
-		return($alephFindXML);	
-	} // end if
-} // end alephFind		
+// Returns XML about a set of resuts. Requires the $findURL from alephXbuildFindURL
+function alephXfind($findURL) {	
+	$findResults = file_get_contents($findURL); // get results of find request
+	$findXML = new SimpleXMLElement($findResults); // turn the results into an XML object
+	return($findXML);	
+} // end alephFind
 
-function alephPresent($alephSetNumber, $setEntry = 1){
-	/*present url gets the actual MARC XML file for the results set
-	 * and loads it into a PHP object
-	 */	
-	$presentURL = "http://catalog.umd.edu/X?set_no=".$alephSetNumber."&set_entry=".$setEntry."&op=present";
+// Returns set number for a result set. Requires $findXML returned from alephXfind()
+function alephXgetSetNum($findXML) {			
+	$setNum = $findXML->set_number;
+	return($setNum);	
+}		
+
+// Returns MarcXML for a record. Default is the first result of a set. Requires the $findURL from alephXbuildFindURL
+function alephXpresent($presentURL, $setEntry = 1){	
 	$presentResults = file_get_contents($presentURL);
-	$alephPresentXML = new SimpleXMLElement($presentResults);
-	return($alephPresentXML); 
+	$presentXML = new SimpleXMLElement($presentResults);
+	return($presentXML); 
 } // end alephPresent
 
+
+
+ 
 ?>    
  
