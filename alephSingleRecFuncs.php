fe3a2fbc782f1934b2e850bf1c309041a2759533
spurioso <spurioso@gmail.com>
2013-09-14 11:33:58 -0400

    Added functions to retrieve MARC and Aleph system number from OCLC number

--- alephSingleRecFuncs.php
+++ alephSingleRecFuncs.php
@@ -2,12 +2,32 @@
 
 include("alephXfunctions.php");
 
+// Tests
+
+/*
+$oclc = "173136007";
+$marcXML = singleRecGetMARCfromOCLC($oclc);
+print_r ($marcXML);
+*/
+ 
+/*
+include("getOCLCnums.php");
+$oclcNums = getOCLCnums($books);
+foreach ($oclcNums as $oclc) {
+	$sysNum = singleRecGetSysNumFromOCLC($oclc);
+	echo "SysNum: ".$sysNum." OCLC: ".$oclc."</br>";
+ 	
+}
+*/
+
+/*
 $sysNum = singleRecGetSysNumFromBar("31430057638498");
 echo $sysNum;
 echo "</br>";
 $marcXML = singleRecGetMARCfromBar("31430057638498");
 print_r($marcXML);
-
+*/
+ 
 /* Returns MarcXML for a single record. Requires a query and the code for the query. If more than one record matches the query
 the first result is returned. Best uses of this function are retrieving a record based on barcode, OCLC number, or Aleph
  * system number. Can also be used for ISBN or call number*/ 
@@ -19,6 +39,18 @@ function alephSingleRec($query, $code) {
 	$presentXML = alephXpresent($presentURL);
 	return($presentXML);	
 }
+
+// Tests to see if a set number was returne (i.e. was the search successful. Probably should move to alephXfunctions)
+function alephTestForSetNum($query, $code) {
+	$findURL = alephXbuildFindURL($query, $code);	
+	$findXML = alephXfind($findURL);	
+	$setNum = alephXgetSetNum($findXML);
+	if ($findXML->set_number) {		
+		return (True);
+	} else {
+		return(False);
+	} // end if	
+} // end alephTestForSetNum
 
 /* *** Functions Returning a Specific MARCXML element *** */
 
@@ -43,5 +75,64 @@ function singleRecGetMARCfromBar($barcode) {
 	$marcXML = alephSingleRec($barcode, "BAR");
 	return ($marcXML);
 }
+
+/* *** Functions For Retrieving Info from an OCLC number *** */
+
+
+function singleRecGetSysNumFromOCLC($oclc) {		
+	$code = "035";
+	if (strlen($oclc) < 8) {
+		if (strlen($oclc) == 6) {
+			$oclcAleph = "00".$oclc;			
+		} elseif (strlen($oclc) == 7) {
+			$oclcAleph = "0".$oclc;
+		}		
+	} else {
+		$oclcAleph = $oclc;
+	}
+	
+	$prefix = "ocm";	
+	$oclcPre = $prefix.$oclcAleph;
+	if ($test = alephTestForSetNum($oclcPre, $code)) {
+		$sysNum = singleRecGetSysNum($oclcPre, $code);
+		return($sysNum);
+	} else {			
+		$prefix = "ocn";		
+		$oclcPre = $prefix.$oclcAleph;		
+		if ($test = alephTestForSetNum($oclcPre, $code)) {
+			$sysNum = singleRecGetSysNum($oclcPre, $code);
+			return($sysNum);			
+		} // end if
+	} // end if
+	
+} // end function
+
+function singleRecGetMARCfromOCLC($oclc) {		
+	$code = "035";
+	if (strlen($oclc) < 8) {
+		if (strlen($oclc) == 6) {
+			$oclcAleph = "00".$oclc;			
+		} elseif (strlen($oclc) == 7) {
+			$oclcAleph = "0".$oclc;
+		}		
+	} else {
+		$oclcAleph = $oclc;
+	}
+	
+	$prefix = "ocm";	
+	$oclcPre = $prefix.$oclcAleph;
+	if ($test = alephTestForSetNum($oclcPre, $code)) {
+		$marcXML = alephSingleRec($oclcPre, $code);
+		return($marcXML);
+	} else {			
+		$prefix = "ocn";		
+		$oclcPre = $prefix.$oclcAleph;		
+		if ($test = alephTestForSetNum($oclcPre, $code)) {
+			$marcXML = alephSingleRec($oclcPre, $code);
+			return($marcXML);
+		} // end if
+	} // end if
+	
+} // end function
 
 ?>
